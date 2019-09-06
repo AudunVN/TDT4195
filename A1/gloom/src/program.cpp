5f22e368fa9ff57d0a0620ee7a52d5ad3112f196
Space <space@471.no>
2019-09-06 14:15:51 +0200

    I HAVE BECOME DRAWER OF TRIANGLES, DESTROYER OF WORLDS

--- A1/gloom/src/program.cpp
+++ A1/gloom/src/program.cpp
@@ -1,7 +1,50 @@
 // Local headers
-#include "program.hpp"
-#include "gloom/gloom.hpp"
+#include <vector>
 
+#include "gloom/gloom.hpp"
+#include "gloom/shader.hpp"
+
+#include "program.hpp"
+
+GLuint vaoGen(std::vector<float> vertices, std::vector<unsigned int> indices) {
+    GLuint array;
+    GLuint vbo_buffer;
+    GLuint index_buffer;
+
+    // generates one VAO
+    glGenVertexArrays(1, &array);
+    glBindVertexArray(array);
+
+    // generates one VBO
+    glGenBuffers(1, &vbo_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_buffer);
+
+    // load data into VBO
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sizeof(int)*vertices.size(),
+        vertices.data(),
+        GL_STATIC_DRAW
+    );
+
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    // generate index array buffer
+    glGenBuffers(1, &index_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+
+    // load data into index array buffer
+    glBufferData(
+        GL_ELEMENT_ARRAY_BUFFER,
+        sizeof(unsigned int)*indices.size(),
+        indices.data(),
+        GL_STATIC_DRAW
+    );
+
+    return array;
+}
 
 void runProgram(GLFWwindow* window)
 {
@@ -16,14 +59,29 @@ void runProgram(GLFWwindow* window)
     glClearColor(0.3f, 0.5f, 0.8f, 1.0f);
 
     // Set up your scene here (create Vertex Array Objects, etc.)
+    std::vector<float> vertices = {
+         0.6f, -0.8f, -1.2f,
+         0.0f,  0.4f,  0.0f,
+        -0.8f, -0.2f,  1.2f,
+    };
+    std::vector<unsigned int> indices = {0, 1, 2};
+    GLuint test_vao = vaoGen(vertices, indices);
+    glBindVertexArray(test_vao);
+
+    printGLError();
+
+    Gloom::Shader shader;
+    shader.makeBasicShader("../../../gloom/shaders/simple.vert","../../../gloom/shaders/simple.frag");
+    shader.activate();
 
     // Rendering Loop
     while (!glfwWindowShouldClose(window))
     {
         // Clear colour and depth buffers
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+        
         // Draw your scene here
+        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
 
         // Handle other events
         glfwPollEvents();
@@ -31,7 +89,11 @@ void runProgram(GLFWwindow* window)
 
         // Flip buffers
         glfwSwapBuffers(window);
+
+        printGLError();
     }
+    shader.deactivate();
+    shader.destroy();
 }
 
 
