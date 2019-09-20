db2c2092f0de9e59849910a69c37e8b256825d77
Space <space@471.no>
2019-09-20 23:50:56 +0200

    Stuck on compilation issues :(

--- A2/gloom/shaders/simple.vert
+++ A2/gloom/shaders/simple.vert
@@ -2,11 +2,12 @@
 
 in vec3 position;
 in layout(location=1) vec4 vertexColor;
+uniform layout(location=7) mat4 transform;
 
 out layout(location=1) vec4 outVertexColor;
 
 void main()
 {
     outVertexColor = vertexColor;
-    gl_Position = vec4(position, 1.0f);
+    gl_Position = vec4(position, 1.0f) * transform;
 }
--- A2/gloom/src/program.cpp
+++ A2/gloom/src/program.cpp
@@ -1,10 +1,11 @@
 // Local headers
 #include <vector>
 
+#include <glm/ext.hpp>
+#include "program.hpp"
+
 #include "gloom/gloom.hpp"
 #include "gloom/shader.hpp"
-
-#include "program.hpp"
 
 GLuint vaoGen(std::vector<float> vertices, std::vector<unsigned int> indices) {
     GLuint array;
@@ -116,40 +117,59 @@ void runProgram(GLFWwindow* window)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     // Set default colour after clearing the colour buffer
-    glClearColor(0.3f, 0.5f, 0.8f, 1.0f);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
     // Set up your scene here (create Vertex Array Objects, etc.)
+    glm::mat4x4 i = mat4(1.0f);
+    glUniformMatrix4fv(7, 1, GL_FALSE, glm::value_ptr(i));
+
     std::vector<float> vertices = {
         /* a triangle */
-         0.6f, -0.8f, -1.2f,
-         0.0f,  0.4f,  0.0f,
-        -0.8f, -0.2f,  1.2f,
+        -0.4f,      0.8f,     0.0f,
+        -0.5f,      0.6f,     0.0f,
+        -0.2636f,   0.5763f,  0.0f,
         /* a triangle */
-         0.5f,  0.5f, 0.0f,
-         0.9f,  0.1f, 0.0f,
-         0.5f,  0.9f, 0.0f,
+         0.1653f,   0.4515f,  0.0f,
+         0.3487f,   0.4955f,  0.0f,
+         0.1377f,   0.2166f,  0.0f,
         /* a triangle */
-        -0.1f, -0.5f, 0.0f,
-        -0.1f, -0.9f, 0.0f,
-        -0.5f, -0.1f, 0.0f,
+         0.4368f,  -0.3099f,  0.0f,
+         0.3781f,  -0.2109f,  0.0f,
+         0.3506f,  -0.2989f,  0.0f,
+        /* a triangle */
+        -0.5870f,  -0.2127f,  0.0f,
+        -0.6237f,  -0.3448f,  0.0f,
+        -0.4806f,  -0.2916f,  0.0f,
+        /* a triangle */
+        -0.1870f,   0.0772f,  0.0f,
+        -0.2586f,  -0.1888f,  0.0f,
+         0.0698f,  -0.1613f,  0.0f,
     };
 
     std::vector<float> colours = {
         /* a triangle */
-        0.1647f, 0.8314f, 1.0f, 1.0f,
+        0.1647f, 0.8314f, 1.0f,    1.0f,
         0.1725f, 0.3529f, 0.6274f, 1.0f,
         0.9921f, 0.9921f, 0.9921f, 1.0f,
         /* a triangle */
-        0.1647f, 0.8314f, 1.0f, 0.2f,
+        0.1647f, 0.8314f, 1.0f,    0.2f,
         0.1725f, 0.3529f, 0.6274f, 0.5f,
         0.9921f, 0.9921f, 0.9921f, 0.8f,
         /* a triangle */
-        0.1647f, 0.8314f, 1.0f, 1.0f,
+        0.1647f, 0.8314f, 1.0f,    1.0f,
+        0.1725f, 0.3529f, 0.6274f, 1.0f,
+        0.9921f, 0.9921f, 0.9921f, 1.0f,
+        /* a triangle */
+        0.1647f, 0.8314f, 1.0f,    1.0f,
+        0.1725f, 0.3529f, 0.6274f, 1.0f,
+        0.9921f, 0.9921f, 0.9921f, 1.0f,
+        /* a triangle */
+        0.1647f, 0.8314f, 1.0f,    1.0f,
         0.1725f, 0.3529f, 0.6274f, 1.0f,
         0.9921f, 0.9921f, 0.9921f, 1.0f,
     };
     
-    std::vector<unsigned int> indices = {0, 1, 2, 4, 3, 5, 6, 7, 8};
+    std::vector<unsigned int> indices = {0, 1, 2, 4, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
     GLuint test_vao = vaoGenWithColour(vertices, indices, colours);
     glBindVertexArray(test_vao);
 
